89bb7753c56d82aabd457664c48b4429d9b0750a
Ian Usher <ian@codeplay.com>
2020-02-20 17:20:00 +0000

    Trying again

--- deploy/travis-deploy.sh
+++ deploy/travis-deploy.sh
@@ -1,4 +1,1 @@
-mkdir $HOME/.m2 -p
-echo "<settings><servers><server><id>github</id><username>FatConan</username><password>$GITHUB_TOKEN</password></server></servers></settings>" > $HOME/.m2/settings.xml
-cat $HOME/.m2/settings.xml
 mvn deploy
---
+++ deploy/travis-setup.sh
@@ -0,0 +1,3 @@
+mkdir $HOME/.m2 -p
+echo "<settings><servers><server><id>github</id><username>FatConan</username><password>$GITHUB_TOKEN</password></server></servers></settings>" > $HOME/.m2/settings.xml
+cat $HOME/.m2/settings.xml
